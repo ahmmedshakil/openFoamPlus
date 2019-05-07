gratienj/openfoam

ENH: ensure that content changes in coded objects are noticed (#1293)
- for codedFunctionObject and CodedSource the main code snippets
  were not included in the SHA1 calculation, which meant that many
  changes would not be noticed and no new library would be compiled.

  As a workaround, a dummy 'code' entry could be used solely for the
  purposes of generating a SHA1, but this is easily forgotten.

  We now allow tracking of the dynamicCodeContext for the coded
  objects and append to the SHA1 hasher with specific entries.
  This should solve the previous misbehaviour.

  We additionally add information about the ordering of the code
  sections. Suppose we have a coded function object (all code
  segments are optional) with the following:

      codeExecute "";
      codeWrite   #{ Info<< "Called\n"; #};

  which we subsequently change to this:

      codeExecute #{ Info<< "Called\n"; #};
      codeWrite   "";

  If the code strings are simply concatenated together, the SHA1 hashes
  will be identical. We thus 'salt' with their semantic locations,
  choosing tags that are unlikely to occur within the code strings
  themselves.

- simplify the coded templates with constexpr for the SHA1sum
  information.

- Correct the CodedSource to use 'codeConstrain' instead of
  'codeSetValue' for consistency with the underlying functions.

etc/codeTemplates/dynamicCode/fixedValueFvPatchFieldTemplate.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2011 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2011, 2019 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
                             | Copyright (C) YEAR AUTHOR,AFFILIATION
@@ -31,6 +31,7 @@ License
 #include "volFields.H"
 #include "surfaceFields.H"
 #include "unitConversion.H"
+
 //{{{ begin codeInclude
 ${codeInclude}
 //}}} end codeInclude
@@ -50,23 +51,20 @@ ${localCode}
 
 // * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
 
-extern "C"
+// dynamicCode:
+// SHA1 = ${SHA1sum}
+//
+// unique function name that can be checked if the correct library version
+// has been loaded
+extern "C" void ${typeName}_${SHA1sum}(bool load)
 {
-    // dynamicCode:
-    // SHA1 = ${SHA1sum}
-    //
-    // unique function name that can be checked if the correct library version
-    // has been loaded
-    void ${typeName}_${SHA1sum}(bool load)
+    if (load)
     {
-        if (load)
-        {
-            // code that can be explicitly executed after loading
-        }
-        else
-        {
-            // code that can be explicitly executed before unloading
-        }
+        // Code that can be explicitly executed after loading
+    }
+    else
+    {
+        // Code that can be explicitly executed before unloading
     }
 }
 
@@ -77,10 +75,6 @@ makeRemovablePatchTypeField
     fvPatch${FieldType},
     ${typeName}FixedValueFvPatch${FieldType}
 );
-
-
-const char* const ${typeName}FixedValueFvPatch${FieldType}::SHA1sum =
-    "${SHA1sum}";
 
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
@@ -96,8 +90,7 @@ ${typeName}FixedValueFvPatch${FieldType}
 {
     if (${verbose:-false})
     {
-        Info<<"construct ${typeName} sha1: ${SHA1sum}"
-            " from patch/DimensionedField\n";
+        printMesssage("Construct ${typeName} : patch/DimensionedField");
     }
 }
 
@@ -115,8 +108,7 @@ ${typeName}FixedValueFvPatch${FieldType}
 {
     if (${verbose:-false})
     {
-        Info<<"construct ${typeName} sha1: ${SHA1sum}"
-            " from patch/DimensionedField/mapper\n";
+        printMesssage("Construct ${typeName} : patch/DimensionedField/mapper");
     }
 }
 
@@ -133,8 +125,7 @@ ${typeName}FixedValueFvPatch${FieldType}
 {
     if (${verbose:-false})
     {
-        Info<<"construct ${typeName} sha1: ${SHA1sum}"
-            " from patch/dictionary\n";
+        printMesssage("Construct ${typeName} : patch/dictionary");
     }
 }
 
@@ -149,8 +140,7 @@ ${typeName}FixedValueFvPatch${FieldType}
 {
     if (${verbose:-false})
     {
-        Info<<"construct ${typeName} sha1: ${SHA1sum}"
-            " as copy\n";
+        printMesssage("Copy construct ${typeName}");
     }
 }
 
@@ -166,8 +156,7 @@ ${typeName}FixedValueFvPatch${FieldType}
 {
     if (${verbose:-false})
     {
-        Info<<"construct ${typeName} sha1: ${SHA1sum} "
-            "as copy/DimensionedField\n";
+        printMesssage("Construct ${typeName} : copy/DimensionedField");
     }
 }
 
@@ -179,7 +168,7 @@ ${typeName}FixedValueFvPatch${FieldType}::
 {
     if (${verbose:-false})
     {
-        Info<<"destroy ${typeName} sha1: ${SHA1sum}\n";
+        printMesssage("Destroy ${typeName}");
     }
 }
 
@@ -195,7 +184,7 @@ void ${typeName}FixedValueFvPatch${FieldType}::updateCoeffs()
 
     if (${verbose:-false})
     {
-        Info<<"updateCoeffs ${typeName} sha1: ${SHA1sum}\n";
+        printMesssage("updateCoeffs ${typeName}");
     }
 
 //{{{ begin code

ahmmedshakil/openFoamPlus

etc/codeTemplates/dynamicCode/fixedValueFvPatchFieldTemplate.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2011 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2011, 2019 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
                             | Copyright (C) YEAR AUTHOR,AFFILIATION
@@ -31,6 +31,7 @@ License
 #include "volFields.H"
 #include "surfaceFields.H"
 #include "unitConversion.H"
+
 //{{{ begin codeInclude
 ${codeInclude}
 //}}} end codeInclude
@@ -50,23 +51,20 @@ ${localCode}
 
 // * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
 
-extern "C"
+// dynamicCode:
+// SHA1 = ${SHA1sum}
+//
+// unique function name that can be checked if the correct library version
+// has been loaded
+extern "C" void ${typeName}_${SHA1sum}(bool load)
 {
-    // dynamicCode:
-    // SHA1 = ${SHA1sum}
-    //
-    // unique function name that can be checked if the correct library version
-    // has been loaded
-    void ${typeName}_${SHA1sum}(bool load)
+    if (load)
     {
-        if (load)
-        {
-            // code that can be explicitly executed after loading
-        }
-        else
-        {
-            // code that can be explicitly executed before unloading
-        }
+        // Code that can be explicitly executed after loading
+    }
+    else
+    {
+        // Code that can be explicitly executed before unloading
     }
 }
 
@@ -77,10 +75,6 @@ makeRemovablePatchTypeField
     fvPatch${FieldType},
     ${typeName}FixedValueFvPatch${FieldType}
 );
-
-
-const char* const ${typeName}FixedValueFvPatch${FieldType}::SHA1sum =
-    "${SHA1sum}";
 
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
@@ -96,8 +90,7 @@ ${typeName}FixedValueFvPatch${FieldType}
 {
     if (${verbose:-false})
     {
-        Info<<"construct ${typeName} sha1: ${SHA1sum}"
-            " from patch/DimensionedField\n";
+        printMessage("Construct ${typeName} : patch/DimensionedField");
     }
 }
 
@@ -115,8 +108,7 @@ ${typeName}FixedValueFvPatch${FieldType}
 {
     if (${verbose:-false})
     {
-        Info<<"construct ${typeName} sha1: ${SHA1sum}"
-            " from patch/DimensionedField/mapper\n";
+        printMessage("Construct ${typeName} : patch/DimensionedField/mapper");
     }
 }
 
@@ -133,8 +125,7 @@ ${typeName}FixedValueFvPatch${FieldType}
 {
     if (${verbose:-false})
     {
-        Info<<"construct ${typeName} sha1: ${SHA1sum}"
-            " from patch/dictionary\n";
+        printMessage("Construct ${typeName} : patch/dictionary");
     }
 }
 
@@ -149,8 +140,7 @@ ${typeName}FixedValueFvPatch${FieldType}
 {
     if (${verbose:-false})
     {
-        Info<<"construct ${typeName} sha1: ${SHA1sum}"
-            " as copy\n";
+        printMessage("Copy construct ${typeName}");
     }
 }
 
@@ -166,8 +156,7 @@ ${typeName}FixedValueFvPatch${FieldType}
 {
     if (${verbose:-false})
     {
-        Info<<"construct ${typeName} sha1: ${SHA1sum} "
-            "as copy/DimensionedField\n";
+        printMessage("Construct ${typeName} : copy/DimensionedField");
     }
 }
 
@@ -179,7 +168,7 @@ ${typeName}FixedValueFvPatch${FieldType}::
 {
     if (${verbose:-false})
     {
-        Info<<"destroy ${typeName} sha1: ${SHA1sum}\n";
+        printMessage("Destroy ${typeName}");
     }
 }
 
@@ -195,7 +184,7 @@ void ${typeName}FixedValueFvPatch${FieldType}::updateCoeffs()
 
     if (${verbose:-false})
     {
-        Info<<"updateCoeffs ${typeName} sha1: ${SHA1sum}\n";
+        printMessage("updateCoeffs ${typeName}");
     }
 
 //{{{ begin code