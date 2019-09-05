ec5c7351e26d79d8740ce0fbd3003bc492a4605b
Kutalmis Bercin <kutalmis.bercin@esi-group.com>
2019-09-05 16:00:16 +0100

    STYLE: add license header and comment lines to Test-tensor2D

--- applications/test/tensor2D/Test-tensor2D.C
+++ applications/test/tensor2D/Test-tensor2D.C
@@ -1,7 +1,34 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2019 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
 #include "tensor2D.H"
 #include "IOstreams.H"
 
 using namespace Foam;
+
+// * * * * * * * * * * * * * * * Main Program  * * * * * * * * * * * * * * * //
 
 int main()
 {
@@ -12,3 +39,6 @@ int main()
 
     return 0;
 }
+
+
+// ************************************************************************* //
